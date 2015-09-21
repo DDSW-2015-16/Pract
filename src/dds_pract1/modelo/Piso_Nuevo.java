8b615a505a37929cde07cefce2fc31045f1bc8d8
aleivaca9.alumnes <aleivaca9.alumnes@ub052056.mat.ub.edu>
2015-09-21 11:57:50 +0200

    Javadoc Piso_Nuevo

--- src/dds_pract1/modelo/Piso_Nuevo.java
+++ src/dds_pract1/modelo/Piso_Nuevo.java
@@ -6,19 +6,30 @@
 package dds_pract1.modelo;
 
 /**
- *
+ * Clase Piso_Nuevo heredera de Piso, la cual posee atributos únicos como porcentaje de beneficio de inmobiliaria
  * @author aleivaca9.alumnes
  */
 public class Piso_Nuevo extends Piso {
     /** int que guarda el porcentaje de beneficio de la inmobiliaria
      */
     int porcentaje;
+    /**
+     * Constructor de la clase Piso_Nuevo la cual nos creará un objeto del tipo Piso_Nuevo
+     * @param precio Precio del piso
+     * @param numero Numero del piso
+     * @param superficie Superficie del piso
+     * @param direccion Direccion donde se encuentra el piso 
+     * @param porcentaje Porcentaje de beneficio para la inmobiliaria
+     */
    public Piso_Nuevo(double precio, int numero, double superficie, String direccion, int porcentaje){
+       /**
+        * Llamamos al constructor de la clase padre Piso
+        */
        super(precio,numero,superficie,direccion);
        porcentaje = this.porcentaje;
        
    }
-
+  
     public int getPorcentaje() {
         return porcentaje;
     }
